8241808f2464ff01cd1bb5cc158fda0d5285da26
vansh123456 <vanshbhatnagar14dev@gmail.com>
2025-05-18 00:41:32 +0530

    add components for the chat ui/ux

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,6 +3,9 @@ import { create } from "zustand";
 import toast from "react-hot-toast";
 import {io} from "socket.io-client";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8080" : "/";
+
+
 
 export const useAuthStore = create((set, get) => ({
 
@@ -90,6 +93,25 @@ export const useAuthStore = create((set, get) => ({
     } finally {
       set({ isUpdatingProfile: false });
     }
-  }
+  },
+  connectSocket: () => {
+    const { authUser } = get();
+    if (!authUser || get().socket?.connected) return;
 
+    const socket = io(BASE_URL, {
+      query: {
+        userId: authUser._id,
+      },
+    });
+    socket.connect();
+
+    set({ socket: socket });
+
+    socket.on("getOnlineUsers", (userIds) => {
+      set({ onlineUsers: userIds });
+    });
+  },
+  disconnectSocket: () => {
+    if (get().socket?.connected) get().socket.disconnect();
+  },
 }));
