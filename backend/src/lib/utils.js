8da96d1da67ffdd5a7203da4a41bf1661596c16c
vansh123456 <vanshbhatnagar14dev@gmail.com>
2025-05-19 15:10:37 +0530

    fix fe baseURL for store and change cookie sec

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -10,8 +10,8 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "lax", // CSRF attacks cross-site request forgery attacks
-    secure: false,
+    sameSite: 'None', // CSRF attacks cross-site request forgery attacks
+    secure: true,
   });
   console.log("Token cookie set:", token);
 
